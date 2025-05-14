c0ab28e7da1c020e5d69fb68fe771aaac641de56
giuseppecasaburi <enzobiondi0000@libero.it>
2025-05-14 23:41:23 +0200

    feat: Update AppFooter and AppHeader components with improved layout and styling, and enhance footer CSS for better responsiveness

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,6 +1,60 @@
+import { NavLink } from "react-router-dom";
+
 function AppFooter() {
-    return(
-        <footer>Footer</footer>
+    const navLinks = [
+        {
+            path: "",
+            title: "HomePage",
+        },
+
+        {
+            path: "/a",
+            title: "Films",
+        },
+
+        {
+            path: "/ss",
+            title: "Registi",
+        },
+    ];
+
+
+    return (
+        <footer>
+            <div
+                id="footer-div"
+                className="container d-flex flex-column flex-sm-row flex-wrap justify-content-between align-items-center text-center"
+            >
+                {/* Colonna 1: Copyright */}
+                <div className="text-center py-3">
+                    <p className="mb-0">© 2025 Company, Inc</p>
+                </div>
+
+                {/* Colonna 2: Logo */}
+                <div className="text-center py-3">
+                    <img src="./img/logo.png" alt="Logo" height="50" />
+                </div>
+
+                {/* Colonna 3: Navigazione */}
+                <div className="text-center py-3">
+                    <ul className="nav d-flex flex-column flex-sm-row justify-content-center">
+                        {navLinks.map((link, index) => (
+                            <li className="nav-item" key={index}>
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        isActive ? "nav-link active text-warning" : "nav-link"
+                                    }
+                                    to={link.path}
+                                    aria-current="page"
+                                >
+                                    {link.title}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </footer>
     )
 }
 
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -24,7 +24,7 @@ function AppHeader() {
                 <nav className="navbar navbar-expand-lg px-3">
                     <div className="container-fluid">
                         <a className="navbar-brand" href={navLinks[0].path}>
-                            <img src="img/logo.png" alt="Bootstrap" width="auto" height="50" />
+                            <img src="img/logo.png" alt="Logo" width="auto" height="50" />
                         </a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
@@ -33,7 +33,7 @@ function AppHeader() {
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 {navLinks.map((link) => (
                                     <li className="nav-item">
-                                        <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} to={link.path} aria-current="page">{link.title}</NavLink>
+                                        <NavLink className={({ isActive }) => isActive ? "nav-link active text-warning" : "nav-link"} to={link.path} aria-current="page">{link.title}</NavLink>
                                     </li>
                                 ))}
 
